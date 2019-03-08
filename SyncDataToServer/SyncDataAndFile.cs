9963124a2ad5b941d70128e029bc0f0cb5b547d2
Ashish Verma <45225991+AshishVerma2107@users.noreply.github.com>
2019-03-08 16:32:20 +0530

    Add files via upload

---
+++ SyncDataToServer/SyncDataAndFile.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Util;
+using ComtaxApp.Database;
+
+namespace ComtaxApp.SyncDataToServer
+{
+    public class SyncDataAndFile
+    {
+        ServiceHelper service;
+        Context mContext;
+        DBHelper dba;
+        InternetConnection con;
+        Geolocation geo;
+
+        public SyncDataAndFile(Context context)
+        {
+            this.mContext = context;
+            service = new ServiceHelper();
+            dba = new DBHelper();
+            con = new InternetConnection();
+            geo = new Geolocation();
+        }
+
+
+        public async Task UploadDataAsync(byte[] photobytes, string photoName)
+        {
+            try
+            {
+              
+            }
+            catch (Exception e)
+            {
+                Log.Error("Error", e.Message);
+                
+            }
+            
+        }
+    }
+}
