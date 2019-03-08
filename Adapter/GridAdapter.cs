02df6c231b702ab37950b2d7fc98e38810904e86
Ashish Verma <45225991+AshishVerma2107@users.noreply.github.com>
2019-03-08 16:34:01 +0530

    Add files via upload

---
+++ Adapter/GridAdapter.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections.Generic;
+using Android.Content;
+using Android.Graphics;
+using Android.Views;
+using Android.Widget;
+using ComtaxApp.Model;
+
+namespace ComtaxApp.Adapter
+{
+    public class GridAdapter : BaseAdapter
+    {
+        public override int Count => myList.Count;
+
+        private Context mContext;
+        public static List<ImageListModel> myList;
+
+        public GridAdapter(Context c, List<ImageListModel> mList)
+        {
+            mContext = c;
+            myList = mList;
+
+
+        }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            View grid;
+            LayoutInflater inflater = (LayoutInflater)mContext
+                    .GetSystemService(Context.LayoutInflaterService);
+
+          
+            grid = new View(mContext);
+            grid = inflater.Inflate(Resource.Layout.grid_layout, null);
+            TextView textView = (TextView)grid.FindViewById(Resource.Id.grid_text);
+            ImageView imageView = (ImageView)grid.FindViewById(Resource.Id.grid_image);
+          
+
+        
+                BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+                bmOptions.InJustDecodeBounds = false;
+                bmOptions.InSampleSize = 4;
+                bmOptions.InPurgeable = true;
+                Bitmap bitmap = BitmapFactory.DecodeFile(myList[position].ImagePath, bmOptions);
+                imageView.SetImageBitmap(bitmap);
+
+         
+            if (convertView != null)
+            {
+                if (myList[position].Status.Equals("1"))
+                {
+                    imageView.SetBackgroundColor(Android.Graphics.Color.AntiqueWhite);// this is a selected position so make it red
+                }
+                else
+                {
+                    imageView.SetBackgroundColor(Android.Graphics.Color.White); //default color
+                }
+            }
+            
+            return grid;
+
+        }
+
+        public void setNewSelection(int position)
+        {
+            myList[position].Status.Equals("1");
+            NotifyDataSetChanged();
+        }
+
+       
+
+
+        public void removeSelection(int position)
+        {
+            myList[position].Status.Equals("0");
+            NotifyDataSetChanged();
+
+        }
+
+       
+    }
+}
---
+++ Adapter/PreviousGSTINAdapter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+using ComtaxApp.Database;
+using ComtaxApp.Model;
+
+namespace ComtaxApp.Adapter
+{
+    public class PreviousGSTINAdapter : BaseAdapter<GSTINModel>
+    {
+        List<GSTINModel> gstins;
+        Context context;
+        Java.IO.File fileName1;
+        ISharedPreferences prefs;
+        DBHelper db;
+
+        public PreviousGSTINAdapter(Context mContext, List<GSTINModel> gstinsList)
+        {
+            this.gstins = gstinsList;
+            this.context = mContext;
+        }
+
+        public override GSTINModel this[int position]
+        {
+            get
+            {
+                return gstins[position];
+            }
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return gstins.Count;
+            }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView;
+           
+            if (view == null)
+            {
+                //view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.layout1, parent, false);
+                view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.previous_gstin_child_layout, null);
+                TextView gstinText = (TextView)view.FindViewById(Resource.Id.gstiNumber);
+                TextView nameText = (TextView)view.FindViewById(Resource.Id.nameTrader);
+                ImageView statusImage = (ImageView)view.FindViewById(Resource.Id.imageViewStatus);
+
+                gstinText.Text = gstins[position].GSTINID;
+                nameText.Text = gstins[position].AppDateTime;
+
+                try
+                {
+                    if (gstins[position].UploadStatus.Equals("yes"))
+                    {
+                        statusImage.SetImageResource(Resource.Drawable.ok_icon);
+                    }
+                    else
+                    {
+                        statusImage.SetImageResource(Resource.Drawable.notok_icon);
+                    }
+
+                }catch(Exception ex)
+                {
+
+                }
+
+
+            }
+          
+           
+            return view;
+
+        }
+
+    }
+}
